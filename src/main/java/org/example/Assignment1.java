019cf8e70a19487cf08207b20337cc277e584b98
Mr-Anubhav-pandey <pandeyanubhav888@gmail.com>
2022-12-09 23:41:36 +0530

    `Assignment 1 `

--- src/main/java/org/example/Assignment1.java
+++ src/main/java/org/example/Assignment1.java
@@ -13,18 +13,118 @@ public class Assignment1
         int half =end/2;
         int quar =half/2;
         // 1.Write a program(WAP) to print INEURON using pattern programming logic.
-        for(int col=start;col<dis;col++)
+        for(int row=start;row<dis;row++)
         {
-            // P
-            if((row==start && col<end) || col==start || (row==half &&col<end) ||
-                    (col==end && row>start && row<half) )
+            for(int col=0;col<dis;col++)
             {
-                System.out.print("*");
+                // I
+                if(col==half && row>=half || col==half && row==quar)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
             }
-            else
+            for(int col=0;col<quar;col++)
             {
                 System.out.print(" ");
             }
+            for(int col=0;col<dis;col++)
+            {
+                // N
+                if(row==col || col==start || col==end)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int col=0;col<quar;col++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=0;col<dis;col++) //E
+            {
+                if(col==0 || row==0 || row==end || (row==half && col< end-quar))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int col=0;col<quar;col++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=0;col<dis;col++) //U
+            {
+                if((col==start && row<end) || (row==end && col>start && col<end) ||
+                        (col==end && row<end))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int col=0;col<quar;col++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=0;col<dis;col++)
+            {
+                // R
+                if((row==start && col<end) || col==start || (row==half &&col<end) ||
+                        (col==end && row>start && row<half) || row==col && row>=half )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int col=0;col<quar;col++)
+            {
+                System.out.print(" ");
+            }
+            //0
+            for(int col=0;col<dis;col++)
+            {
+                if(col==start & row>start && row<end || col ==end && row>start && row<end ||
+                        row==start && col>start && col<end || row==end && col>start&&col<end)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            for(int col=0;col<quar;col++)
+            {
+                System.out.print(" ");
+            }
+            for(int col=0;col<dis;col++)
+            {
+                // N
+                if(row==col || col==start || col==end)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 
